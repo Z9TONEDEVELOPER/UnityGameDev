32e2a1d69440c389fae02cf2af2ebbe3bb928834
dimaten2 <yariktendom@gmail.com>
2025-09-15 15:42:46 +0300

    Add TestScript for testing GIT

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System;
+public class TestScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Console.Write();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
